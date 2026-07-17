6c948b880da1a033435fa0228fe3e82e0d020422
alex osorio <ale.osoriov@duocuc.cl>
2026-07-17 17:13:21 -0400

    Conecta cargador privado de viviendas al Superadministrador

--- app/superadmin-app.tsx
+++ app/superadmin-app.tsx
@@ -2,11 +2,13 @@
 
 import RouteApp from "./route-app";
 import SuperadminConsole from "./superadmin-console";
+import SuperadminRouteManager from "./superadmin-route-manager";
 
 export default function SuperadminApp() {
   return (
     <>
       <SuperadminConsole />
+      <SuperadminRouteManager />
       <RouteApp />
     </>
   );
